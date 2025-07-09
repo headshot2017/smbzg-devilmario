a3c90d52e7e3dc7dca33324a3e861919003667f5
headshot2017 <>
2025-07-09 03:45:52 -0400

    move custom Koopa Bros code to CharLoader

--- Core.cs
+++ Core.cs
@@ -40,55 +40,6 @@ namespace DevilMarioMod
             devilBoo.cc = cc;
             devilBoo.enabled = true;
             GameObject.Destroy(old);
-
-            // modify koopa bros to use a custom class
-            // so when possessed, it'll follow Devil Mario
-            GameObject KoopaBrosPrefab = BattleCache.ins.CharacterData_KoopaBros.Prefab_BattleGameObject;
-
-            KoopaRedControl original = KoopaBrosPrefab.GetComponent<KoopaRedControl>();
-            CustomKoopaRedControl custom = KoopaBrosPrefab.AddComponent<CustomKoopaRedControl>();
-            /*
-            foreach (PropertyInfo property in original.GetType().GetProperties(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-            {
-                try
-                {
-                    custom.GetType().BaseType.GetProperty(property.Name, BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(custom, property.GetValue(original));
-                }
-                catch (Exception)
-                {
-
-                }
-            }
-            */
-            foreach (FieldInfo field in original.GetType().GetFields(BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-            {
-                try
-                {
-                    custom.GetType().BaseType.GetField(field.Name, BindingFlags.Static | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(custom, field.GetValue(original));
-                }
-                catch (Exception)
-                {
-
-                }
-            }
-
-
-            KoopaBroControl originalBro = original.KoopaBro_Prefab.GetComponent<KoopaBroControl>();
-            CustomKoopaBroControl customBro = original.KoopaBro_Prefab.AddComponent<CustomKoopaBroControl>();
-            foreach (FieldInfo field in originalBro.GetType().GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic))
-            {
-                try
-                {
-                    customBro.GetType().BaseType.GetField(field.Name, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).SetValue(customBro, field.GetValue(originalBro));
-                }
-                catch (Exception)
-                {
-
-                }
-            }
-
-            GameObject.Destroy(originalBro);
-            GameObject.Destroy(original);
         }
 
         void ResetBattleParticipant(BattleParticipantDataModel participant)
